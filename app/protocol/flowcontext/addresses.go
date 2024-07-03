13b829832d6898a96e433fe47d53e9bf218a24a6
sad <sadnetwork@hotmail.com>
2024-07-03 13:18:29 +0700

    Create addresses.go

---
+++ app/protocol/flowcontext/addresses.go
@@ -0,0 +1,10 @@
+package flowcontext
+
+import (
+	"github.com/sadnetwork/sad/infrastructure/network/addressmanager"
+)
+
+// AddressManager returns the address manager associated to the flow context.
+func (f *FlowContext) AddressManager() *addressmanager.AddressManager {
+	return f.addressManager
+}
