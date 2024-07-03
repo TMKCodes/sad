e04c1fbf57b38ba9dda54cbbbe81cef93646e3d1
sad <sadnetwork@hotmail.com>
2024-07-03 13:23:01 +0700

    Create receiveaddresses.go

---
+++ app/protocol/flows/v5/addressexchange/receiveaddresses.go
@@ -0,0 +1,39 @@
+package addressexchange
+
+import (
+	"github.com/sadnetwork/sad/app/appmessage"
+	"github.com/sadnetwork/sad/app/protocol/common"
+	peerpkg "github.com/sadnetwork/sad/app/protocol/peer"
+	"github.com/sadnetwork/sad/app/protocol/protocolerrors"
+	"github.com/sadnetwork/sad/infrastructure/network/addressmanager"
+	"github.com/sadnetwork/sad/infrastructure/network/netadapter/router"
+)
+
+// ReceiveAddressesContext is the interface for the context needed for the ReceiveAddresses flow.
+type ReceiveAddressesContext interface {
+	AddressManager() *addressmanager.AddressManager
+}
+
+// ReceiveAddresses asks a peer for more addresses if needed.
+func ReceiveAddresses(context ReceiveAddressesContext, incomingRoute *router.Route, outgoingRoute *router.Route,
+	peer *peerpkg.Peer) error {
+
+	subnetworkID := peer.SubnetworkID()
+	msgGetAddresses := appmessage.NewMsgRequestAddresses(false, subnetworkID)
+	err := outgoingRoute.Enqueue(msgGetAddresses)
+	if err != nil {
+		return err
+	}
+
+	message, err := incomingRoute.DequeueWithTimeout(common.DefaultTimeout)
+	if err != nil {
+		return err
+	}
+
+	msgAddresses := message.(*appmessage.MsgAddresses)
+	if len(msgAddresses.AddressList) > addressmanager.GetAddressesMax {
+		return protocolerrors.Errorf(true, "address count exceeded %d", addressmanager.GetAddressesMax)
+	}
+
+	return context.AddressManager().AddAddresses(msgAddresses.AddressList...)
+}
