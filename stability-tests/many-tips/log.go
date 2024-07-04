0c2d47f04c3d1c24705b1e695e23d99a2094d37a
sad <sadnetwork@hotmail.com>
2024-07-04 22:22:21 +0700

    Add files via upload

---
+++ stability-tests/many-tips/log.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sadnetwork/sad/infrastructure/logger"
+	"github.com/sadnetwork/sad/stability-tests/common"
+	"github.com/sadnetwork/sad/util/panics"
+	"os"
+)
+
+var (
+	backendLog = logger.NewBackend()
+	log        = backendLog.Logger("MATS")
+	spawn      = panics.GoroutineWrapperFunc(log)
+)
+
+func initLog(logFile, errLogFile string) {
+	level := logger.LevelDebug
+	if activeConfig().LogLevel != "" {
+		var ok bool
+		level, ok = logger.LevelFromString(activeConfig().LogLevel)
+		if !ok {
+			fmt.Fprintf(os.Stderr, "Log level %s doesn't exists", activeConfig().LogLevel)
+			os.Exit(1)
+		}
+	}
+	log.SetLevel(level)
+	common.InitBackend(backendLog, logFile, errLogFile)
+}
