86820a057b2967b593e60f554c53053a2b8dcc42
sad <sadnetwork@hotmail.com>
2024-07-04 21:21:38 +0700

    Add files via upload

---
+++ infrastructure/network/rpcclient/log.go
@@ -0,0 +1,9 @@
+package rpcclient
+
+import (
+	"github.com/sadnetwork/sad/infrastructure/logger"
+	"github.com/sadnetwork/sad/util/panics"
+)
+
+var log = logger.RegisterSubSystem("RPCC")
+var spawn = panics.GoroutineWrapperFunc(log)
