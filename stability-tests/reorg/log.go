a2d8b8d888cc9a39b9479ac1bc36379c4affeacc
sad <sadnetwork@hotmail.com>
2024-07-04 22:00:06 +0700

    Add files via upload

---
+++ stability-tests/reorg/log.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/sadnetwork/sad/infrastructure/logger"
+	"github.com/sadnetwork/sad/util/panics"
+)
+
+var (
+	backendLog = logger.NewBackend()
+	log        = backendLog.Logger("RORG")
+	spawn      = panics.GoroutineWrapperFunc(log)
+)
