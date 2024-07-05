88fee1d78a9ef8837f41d453f56743e2285b5be8
sad <sadnetwork@hotmail.com>
2024-07-05 11:54:20 +0700

    Update netconnection.go

--- infrastructure/network/netadapter/netconnection.go
+++ infrastructure/network/netadapter/netconnection.go
@@ -2,13 +2,13 @@ package netadapter
 
 import (
 	"fmt"
-	"github.com/sedracoin/sedrad/app/appmessage"
-	routerpkg "github.com/sedracoin/sedrad/infrastructure/network/netadapter/router"
+	"github.com/sadnetwork/sad/app/appmessage"
+	routerpkg "github.com/sadnetwork/sad/infrastructure/network/netadapter/router"
 	"github.com/pkg/errors"
 	"sync/atomic"
 
-	"github.com/sedracoin/sedrad/infrastructure/network/netadapter/id"
-	"github.com/sedracoin/sedrad/infrastructure/network/netadapter/server"
+	"github.com/sadnetwork/sad/infrastructure/network/netadapter/id"
+	"github.com/sadnetwork/sad/infrastructure/network/netadapter/server"
 )
 
 // NetConnection is a wrapper to a server connection for use by services external to NetAdapter
